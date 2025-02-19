e6be9694f0dbcedb6d5de32b1f8d96ddcb38ec54
hettfield git <olekpl90@gmail.com>
2025-02-19 18:57:36 +0100

    added title in main section with photo

--- inc/acf/blocks/main-section-block/main-section-block.php
+++ inc/acf/blocks/main-section-block/main-section-block.php
@@ -2,6 +2,9 @@
 $default_classes = [
     'main_photo' => 'main_photo',
     'image-container' => 'image-container',
+    'upper-title-wrapper' => 'upper-title-wrapper',
+    'title' => 'title',
+    'title-container' => 'title-container',
 ];
 
 $modules_file = get_template_directory() . '/assets/blocks/styles/modules.json';
@@ -13,12 +16,24 @@ if (file_exists($modules_file)) {
 }
 ?>
 
-<div class='container'>
-    <?php 
-        $image = get_field('main_section_photo');
-        if( !empty( $image ) ): ?>
-            <div class="<?php echo esc_attr($classes['image-container']); ?>">
-                <img class="<?php echo esc_attr($classes['main_photo']); ?>" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+<section class='section'>
+    <div class='container'>
+        <!-- If title is above the image -->
+        <?php 
+            $main_section_upper_title = get_field('main_section_upper_title');
+            if (!empty($main_section_upper_title)) : 
+        ?>
+            <div class="<?php echo esc_attr($classes['upper-title-wrapper']); ?>">
+                <h2 class="<?php echo esc_attr($classes['title']); ?>"><?php echo esc_html($main_section_upper_title); ?></h2>
             </div>
-    <?php endif; ?>
-</div>
+        <?php endif; ?>
+        <!-- Image -->
+        <?php 
+            $image = get_field('main_section_photo');
+            if( !empty( $image ) ): ?>
+                <div class="<?php echo esc_attr($classes['image-container']); ?>">
+                    <img class="<?php echo esc_attr($classes['main_photo']); ?>" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                </div>
+        <?php endif; ?>
+    </div>
+</section>
