402e6bbe31482972ab72a960e15beb0d4263a78c
hettfield git <olekpl90@gmail.com>
2025-02-20 16:34:14 +0100

    added partners-block test content

--- inc/acf/blocks/partners-block/partners-block.php
+++ inc/acf/blocks/partners-block/partners-block.php
@@ -1,10 +1,6 @@
 <?php
 $default_classes = [
-    'main_photo' => 'main_photo',
-    'image-container' => 'image-container',
-    'upper-title-wrapper' => 'upper-title-wrapper',
-    'title' => 'title',
-    'title-container' => 'title-container',
+    'partners-info' => 'partners-info',
 ];
 
 $modules_file = get_template_directory() . '/assets/blocks/styles/modules.json';
@@ -12,28 +8,19 @@ $classes = $default_classes;
 
 if (file_exists($modules_file)) {
     $modules = json_decode(file_get_contents($modules_file), true);
-    $classes = array_merge($default_classes, $modules['main-section-block'] ?? []);
+    $classes = array_merge($default_classes, $modules['partners-block'] ?? []);
 }
 ?>
 
 <section class='section'>
     <div class='container'>
-        <!-- If title is above the image -->
         <?php 
-            $main_section_upper_title = get_field('main_section_upper_title');
-            if (!empty($main_section_upper_title)) : 
+            $text = get_field('partners_text');
+            if (!empty($text)) : 
         ?>
-            <div class="<?php echo esc_attr($classes['upper-title-wrapper']); ?>">
-                <h2 class="<?php echo esc_attr($classes['title']); ?>"><?php echo esc_html($main_section_upper_title); ?></h2>
+            <div class="<?php echo esc_attr($classes['partners-info']); ?>">
+                <p class="<?php echo esc_attr($classes['partners-info']); ?>"><?php echo esc_html($text); ?></p>
             </div>
-        <?php endif; ?>
-        <!-- Image -->
-        <?php 
-            $image = get_field('main_section_photo');
-            if( !empty( $image ) ): ?>
-                <div class="<?php echo esc_attr($classes['image-container']); ?>">
-                    <img class="<?php echo esc_attr($classes['main_photo']); ?>" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                </div>
         <?php endif; ?>
     </div>
 </section>
