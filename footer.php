37b82f9b3ba758ee964a7f817113ea44d944e9c9
NilKad <kadulin@gmail.com>
2025-02-21 19:54:53 +0100

    footer final fix

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
 <?php
 $ornament_url = get_template_directory_uri() . "/assets/images/symbol-defs.svg#icon-ornament1";
-$phone_number = '+38 099 169 47 45';
+// $phone_number = '+38 099 169 47 45';
+$phone_number = esc_html(get_field('phone_number', 'option'));
 $phone = str_replace(' ', '', $phone_number);
+$email = esc_html(get_field('email', 'option'));
+
 $socials = get_field('socials', 'option');
-echo var_dump($socials);
+// echo var_dump($socials);
 ?>
 <style>
     /* https://nachinnya.loc/wp-content/themes/nachinnya/assets/images/symbol-defs.svg#icon-ornament1 */
@@ -55,10 +58,10 @@ echo var_dump($socials);
             <address class="address">
                 <div class="address-contact">
                     <div class="address-wrap ">
-                        <a href="mailto:INFO@NACHYNNYA.ORG.UA" target="_blank" class="email link-adv">iNFO@NACHYNNYA.ORG.UA</a>
+                        <a href="mailto:<?php echo $email; ?>" target="_blank" class="email link-adv"><?php echo $email; ?></a>
                     </div>
                     <div class="address-wrap ">
-                        <a href="tel:<?php echo $phone; ?>" class="phone link-adv"><?php echo $phone_number ?></a>
+                        <a href="tel:<?php echo $phone; ?>" class="phone link-adv"><?php echo $phone_number; ?></a>
                     </div>
                 </div>
                 <div class="uzor">
@@ -69,36 +72,28 @@ echo var_dump($socials);
                         <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-wave-l-y"></use>
                     </svg>
                 </div>
+                <?php
+                $socseti = get_field('socseti_block', 'options');
+                $socseti_title = esc_html(get_field('socseti_title', 'options'));
+                // echo var_dump($socseti);
+                ?>
                 <div class="social-wrap">
-                    <p class="social-title">МИ У СОЦМЕРЕЖАХ:</p>
-                    <div class="social footer__social">
-                        <a href="#" class="social-link">
-                            <svg class="social-icon">
-                                <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-instagram"></use>
-                            </svg>
-                        </a>
-                        <a href="#" class="social-link">
-                            <svg class="social-icon">
-                                <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-facebook"></use>
-                            </svg>
-                        </a>
-                        <a href="#" class="social-link">
-                            <svg class="social-icon">
-                                <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-tiktok"></use>
-                            </svg>
-                        </a>
-                        <a href="#" class="social-link">
-                            <svg class="social-icon">
-                                <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-linkedin"></use>
-                            </svg>
-                        </a>
-                        <a href="#" class="social-link">
-                            <svg class="social-icon">
-                                <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-youtube"></use>
-                            </svg>
-                        </a>
-                    </div>
+                    <?php if ($socseti) : ?>
+                        <p class="social-title"><?php echo $socseti_title; ?></p>
+                        <div class="social footer__social">
+                            <?php foreach ($socseti as $item) : ?>
+                                <a href="<?php echo $item['url']; ?>" class="social-link">
+                                    <?php
+                                    $social_sprite_url = esc_url(get_field('social_sprite', 'options')) . "#" . $item['sprite_icon_name'];
+                                    ?>
+                                    <svg class="social-icon">
+                                        <use xlink:href="<?php echo $social_sprite_url; ?>"></use>
+                                    </svg>
+                                </a>
+                            <?php endforeach; ?>
 
+                        </div>
+                    <?php endif; ?>
                 </div>
             </address>
 
@@ -106,8 +101,7 @@ echo var_dump($socials);
 
         <!-- <img src="" alt="" class="footer-ornament"> -->
         <div class="copyright-wrap">
-            <a href="https://it-volunteers.com/" class="copyright link-adv" target="_blank">Сайт розроблено IT Volunteers ©2025 Громадська організація «Начиння»</a>
-
+            <a href="https://it-volunteers.com/" class="copyright link-adv" target="_blank"><?php echo esc_html(get_field('copyright', 'option')); ?></a>
         </div>
     </div>
 </footer>
