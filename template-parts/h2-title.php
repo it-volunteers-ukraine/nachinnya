ac72c74cf268a7ef3f11ec97bd16926a1d01f80c
hettfield git <olekpl90@gmail.com>
2025-02-23 19:15:46 +0100

    fix

--- template-parts/h2-title.php
+++ template-parts/h2-title.php
@@ -1,7 +1,10 @@
-<?php 
-    $current_id = get_the_ID();
-    $h2_title = get_field('h2_title', $current_id);
-    if (!empty($h2_title)) : 
+<?php
+
+wp_enqueue_style('h2-title-style', get_template_directory_uri() . '/assets/styles/template-parts/h2-title.css', [], '1.0');
+
+$h2_title = get_field('h2_title');
+
+if (!empty($h2_title)) :
 ?>
     <div class="h2-title-wrapper">
         <h2 class="h2-title"><?php echo esc_html($h2_title); ?></h2>
