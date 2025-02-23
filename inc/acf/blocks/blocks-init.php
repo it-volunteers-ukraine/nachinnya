bb7482c85ddae0edb04c4ae7892374f9b9cd50f3
hettfield git <olekpl90@gmail.com>
2025-02-23 21:55:09 +0100

    deleted registered blocks

--- inc/acf/blocks/blocks-init.php
+++ inc/acf/blocks/blocks-init.php
@@ -34,17 +34,6 @@ function it_volunteers_acf_blocks_init() {
                 include_once $register_file;
             }
         }
-        
-        // Register a hero1 block.
-        acf_register_block_type(array(
-            'name'              => 'hero1',
-            'title'             => __('Hero1'),
-            'description'       => __('Block Hero1'),
-            'render_template'   => acf_theme_blocks_path('hero1/hero1.php'),
-            'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/hero1/hero1.css',
-            'enqueue_script'    => get_template_directory_uri() . '/assets/blocks/scripts/hero1/hero1.js',
-            'category'          => 'custom-blocks',
-        ));
 
         // Register block for rendering the latest posts
         acf_register_block_type(array(
@@ -57,39 +46,6 @@ function it_volunteers_acf_blocks_init() {
             'category'          => 'posts-blocks',
             'icon'              => 'admin-post',
             'keywords'          => array('posts', 'latest posts', 'blog'),
-        ));
-
-        // Register a jsblock1 block
-        acf_register_block_type(array(
-            'name'              => 'jsblock1',
-            'title'             => __('jsblock1'),
-            'description'       => __('Block jsblock1'),
-            'render_template'   => acf_theme_blocks_path('jsblock1/jsblock1.php'),
-            'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/jsblock1/jsblock1.css',
-            'enqueue_script'    => get_template_directory_uri() . '/assets/blocks/scripts/jsblock1/jsblock1.js',
-            'category'          => 'custom-blocks',
-        ));
-
-        // Register a jsblock2 block
-        acf_register_block_type(array(
-            'name'              => 'jsblock2',
-            'title'             => __('jsblock2'),
-            'description'       => __('Block jsblock2'),
-            'render_template'   => acf_theme_blocks_path('jsblock2/jsblock2.php'),
-            'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/jsblock2/jsblock2.css',
-            'enqueue_script'    => get_template_directory_uri() . '/assets/blocks/scripts/jsblock2/jsblock2.js',
-            'category'          => 'custom-blocks',
-        ));
-
-        // Register a first-block
-        acf_register_block_type(array(
-            'name'              => 'first-block',
-            'title'             => __('first-block'),
-            'description'       => __('Block first-block'),
-            'render_template'   => acf_theme_blocks_path('first-block/first-block.php'),
-            'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/first-block/first-block.css',
-            'enqueue_script'    => get_template_directory_uri() . '/assets/blocks/scripts/first-block/first-block.js',
-            'category'          => 'custom-blocks',
         ));
 
         // Register main-section-block
