5c7ff3539a0d93ff86ce2b8e0deb56cb77d30868
NilKad <kadulin@gmail.com>
2025-02-23 23:10:35 +0100

    after merge

--- inc/acf/blocks/blocks-init.php
+++ inc/acf/blocks/blocks-init.php
@@ -1,11 +1,14 @@
 <?php
 
-function acf_theme_blocks_path($path = '') {
+function acf_theme_blocks_path($path = '')
+{
     return get_template_directory() . '/inc/acf/blocks/' . $path;
 }
 
-function gt_block_category_init( $categories, $post ) {
-    return array_merge([
+function gt_block_category_init($categories, $post)
+{
+    return array_merge(
+        [
             [
                 'slug' => 'custom-blocks',
                 'title' => __('Custom Blocks', 'it_volunteers_blocks_theme'),
@@ -18,18 +21,19 @@ function gt_block_category_init( $categories, $post ) {
         $categories
     );
 }
-add_filter( 'block_categories', 'gt_block_category_init', 10, 2 );
+add_filter('block_categories', 'gt_block_category_init', 10, 2);
 
 /* Register acf blocks */
-function it_volunteers_acf_blocks_init() {
-    
+function it_volunteers_acf_blocks_init()
+{
+
     // Check function exists.
-    if( function_exists('acf_register_block_type') ) {
+    if (function_exists('acf_register_block_type')) {
 
         $block_folders = glob(acf_theme_blocks_path() . '*', GLOB_ONLYDIR);
         foreach ($block_folders as $block_path) {
             $register_file = $block_path . '/register.php';
-    
+
             if (file_exists($register_file)) {
                 include_once $register_file;
             }
@@ -78,6 +82,17 @@ function it_volunteers_acf_blocks_init() {
             'description'       => __('Breadcrumbs'),
             'render_template'   => acf_theme_blocks_path('breadcrumbs/breadcrumbs.php'),
             'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/breadcrumbs/breadcrumbs.css',
+            'category'          => 'custom-blocks',
+        ));
+
+        // Register a team block
+        acf_register_block_type(array(
+            'name'              => 'teams-block',
+            'title'             => __('Teams Block'),
+            'description'       => __('Block Teams'),
+            'render_template'   => acf_theme_blocks_path('teams-block/teams-block.php'),
+            'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/teams-block/teams-block.css',
+            // 'enqueue_script'    => get_template_directory_uri() . '/assets/blocks/scripts/teams-block/teams-block.js',
             'category'          => 'custom-blocks',
         ));
     }
