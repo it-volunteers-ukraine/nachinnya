faea774126b3dc2c98e8286e42c41e9b87097c40
hettfield git <olekpl90@gmail.com>
2025-02-23 21:19:25 +0100

    fixed h2 template-part styles

--- template-parts/h2-title.php
+++ template-parts/h2-title.php
@@ -7,6 +7,8 @@ $h2_title = get_field('h2_title');
 if (!empty($h2_title)) :
 ?>
     <div class="h2-title-wrapper">
-        <h2 class="h2-title"><?php echo esc_html($h2_title); ?></h2>
+        <div class="h2-title-elements-container">
+            <h2 class="h2-title"><?php echo esc_html($h2_title); ?></h2>
+        </div>
     </div>
 <?php endif; ?>
