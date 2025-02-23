bd76c2019b28c8a2a9d5c0481e4c0fc4bb30d250
NilKad <kadulin@gmail.com>
2025-02-23 19:32:29 +0100

    v0

--- inc/acf/blocks/teams-block/teams-block.php
+++ inc/acf/blocks/teams-block/teams-block.php
@@ -1,7 +1,14 @@
+<?php 
+wp_enqueue_style('h2-title-style', get_template_directory_uri() . '/assets/styles/template-parts-styles/h2-title.css', array('main'));
+$current_id = get_the_ID();
+// echo $current_id;
+// $aaa = get_field('h2_title');
+?>
+
 <section class="section">
     <div class="container">
-        sasasdasds
-        <!-- <?php echo get_field('h2_title'); ?> -->
+        <?php get_template_part('template-parts/h2-title', null, []); ?>
+
     </div>
 
 </section>
