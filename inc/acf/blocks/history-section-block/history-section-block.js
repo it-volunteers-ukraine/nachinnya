38d9fa685699a0673e17f4658e62229ba1f3b82a
hettfield git <olekpl90@gmail.com>
2025-02-24 09:52:43 +0100

    precised classes in script with using styles modules

--- inc/acf/blocks/history-section-block/history-section-block.js
+++ inc/acf/blocks/history-section-block/history-section-block.js
@@ -1,14 +1,23 @@
 
+const balls = document.querySelectorAll('[class*="history-section-block-module__ball-container"]');
+const trajectories = document.querySelectorAll('[class*="history-section-block-module__path-container"]');
+
 gsap.registerPlugin(MotionPathPlugin);
 
-gsap.to(".ball-container svg", {
-    motionPath: {
-        path: ".path-container path",
-        align: ".path-container path",
-        autoRotate: true,
-        alignOrigin: [0.5, 0.5]
-    },
-    duration: 15,
-    repeat: -1,
-    ease: "power1.inOut"
+balls.forEach((ball, index) => {
+    const trajectory = trajectories[index]?.querySelector("path");
+
+    if (trajectory) {
+        gsap.to(ball.querySelector("svg"), {
+            motionPath: {
+                path: trajectory,
+                align: trajectory,
+                autoRotate: true,
+                alignOrigin: [0.5, 0.5]
+            },
+            duration: 15,
+            repeat: -1,
+            ease: "power1.inOut"
+        });
+    }
 });
