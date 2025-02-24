9af378becdcf4ac9ba8ec6e4b1460121b5222588
NilKad <kadulin@gmail.com>
2025-02-24 21:17:22 +0100

    v1_photo_frame

--- inc/acf/blocks/blocks-init.php
+++ inc/acf/blocks/blocks-init.php
@@ -84,17 +84,6 @@ function it_volunteers_acf_blocks_init()
             'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/breadcrumbs/breadcrumbs.css',
             'category'          => 'custom-blocks',
         ));
-
-        // Register a team block
-        acf_register_block_type(array(
-            'name'              => 'teams-block',
-            'title'             => __('Teams Block'),
-            'description'       => __('Block Teams'),
-            'render_template'   => acf_theme_blocks_path('teams-block/teams-block.php'),
-            'enqueue_style'     => get_template_directory_uri() . '/assets/blocks/styles/teams-block/teams-block.css',
-            // 'enqueue_script'    => get_template_directory_uri() . '/assets/blocks/scripts/teams-block/teams-block.js',
-            'category'          => 'custom-blocks',
-        ));
     }
 }
 add_action('acf/init', 'it_volunteers_acf_blocks_init');
