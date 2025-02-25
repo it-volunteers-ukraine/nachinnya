3bef3a62b6cab302f77c3b518d02debaa64d5e5b
NilKad <kadulin@gmail.com>
2025-02-26 00:09:46 +0100

    final photo vert & horiz

--- template-parts/photo-horizontal.php
+++ template-parts/photo-horizontal.php
@@ -1,12 +1,17 @@
 <?php
 wp_enqueue_style('parts-photo-horizontal-style', get_template_directory_uri() . '/assets/styles/template-parts/photo-horizontal.css', [], '1.0');
 
-$photo_url = $args['photo_url'];
-$photo_alt = $args['photo_alt'];
+// $photo_url = $args['photo_url'];
+// $photo_url = isset($args['photo_url']) ? $args['photo_url'] : get_template_directory_uri() . '/assets/images/no_image_available_500_x_500.svg';
+// $photo_alt = $args['photo_alt'];
+$photo_url = isset($args['photo_url']) ? $args['photo_url'] : get_template_directory_uri() . '/assets/images/no_image_available_500_x_500.svg';
+$photo_alt = isset($args['photo_alt']) ? $args['photo_alt'] : 'alt';
+$my_class = isset($args['my_class']) ? $args['my_class'] : '';
+
 ?>
 
-<div class='team-img-frame-hor'>
-    <div class='team-img-wrap-hor'>
-        <img src="<?php echo $photo_url; ?>" class="team-img" alt="<?php echo $photo_alt; ?>" ?>
+<div class='hor-img-frame <?php echo $my_class; ?>'>
+    <div class='hor-img-wrap'>
+        <img src="<?php echo $photo_url; ?>" class="hor-img " alt="<?php echo $photo_alt; ?>" ?>
     </div>
 </div>
--- template-parts/photo-vertical.php
+++ template-parts/photo-vertical.php
@@ -1,12 +1,16 @@
 <?php
 wp_enqueue_style('parts-photo-vertical-style', get_template_directory_uri() . '/assets/styles/template-parts/photo-vertical.css', [], '1.0');
 
-$photo_url = $args['photo_url'];
-$photo_alt = $args['photo_alt'];
+// $photo_url = $args['photo_url'];
+// $photo_alt = $args['photo_alt'];
+
+$photo_url = isset($args['photo_url']) ? $args['photo_url'] : get_template_directory_uri() . '/assets/images/no_image_available_500_x_500.svg';
+$photo_alt = isset($args['photo_alt']) ? $args['photo_alt'] : 'alt';
+$my_class = isset($args['my_class']) ? $args['my_class'] : '';
 ?>
 
-<div class='team-img-frame'>
-    <div class='team-img-wrap'>
-        <img src="<?php echo $photo_url; ?>" class="team-img" alt="<?php echo $photo_alt; ?>" ?>
+<div class='vert-img-frame <?php echo $my_class; ?>'>
+    <div class='vert-img-wrap'>
+        <img src="<?php echo $photo_url; ?>" class="vert-img " alt="<?php echo $photo_alt; ?>" ?>
     </div>
 </div>
