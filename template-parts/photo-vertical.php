9b31bd91d8fe74cbb214bc026130860959ff7b08
NilKad <kadulin@gmail.com>
2025-02-26 00:21:51 +0100

    update vert & hor photo

--- template-parts/photo-vertical.php
+++ template-parts/photo-vertical.php
@@ -1,9 +1,6 @@
 <?php
 wp_enqueue_style('parts-photo-vertical-style', get_template_directory_uri() . '/assets/styles/template-parts/photo-vertical.css', [], '1.0');
 
-// $photo_url = $args['photo_url'];
-// $photo_alt = $args['photo_alt'];
-
 $photo_url = isset($args['photo_url']) ? $args['photo_url'] : get_template_directory_uri() . '/assets/images/no_image_available_500_x_500.svg';
 $photo_alt = isset($args['photo_alt']) ? $args['photo_alt'] : 'alt';
 $my_class = isset($args['my_class']) ? $args['my_class'] : '';
