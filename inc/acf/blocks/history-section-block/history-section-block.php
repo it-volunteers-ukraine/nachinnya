26e60c75e1b445099be1306a0f1c360a7513f02d
hettfield git <olekpl90@gmail.com>
2025-03-04 17:39:53 +0100

    moved script to functions

--- inc/acf/blocks/history-section-block/history-section-block.php
+++ inc/acf/blocks/history-section-block/history-section-block.php
@@ -201,7 +201,4 @@ if (file_exists($modules_file)) {
             </div>
         </div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/MotionPathPlugin.min.js"></script>
 </section>
