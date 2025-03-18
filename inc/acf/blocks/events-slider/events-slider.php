e46527fd5d5bc4ea2c2bce33604b76e11c6319a9
Andrii Katsubo <andrii.katsubo@gmail.com>
2025-03-18 07:47:37 +0200

    Save the current progress to the repo

--- inc/acf/blocks/events-slider/events-slider.php
+++ inc/acf/blocks/events-slider/events-slider.php
@@ -3,17 +3,37 @@
     $default_classes = [
         'section' => 'section',
         'container' => 'container',
+        'title' => 'title',
+        'title-header' => 'title-header',
+        'title-header-image' => 'title-header-image',
+        'title-header-text' => 'title-header-text',
+        'title-decoration-image' => 'title-decoration-image',
     ];
 
     $modules_file = get_template_directory() . '/assets/blocks/styles/modules.json';
     $classes = $default_classes;
     if (file_exists($modules_file)) {
         $modules = json_decode(file_get_contents($modules_file), true);
-        $classes = array_merge($default_classes, $modules['events'] ?? []);
+        $classes = array_merge($default_classes, $modules['events-slider'] ?? []);
     }
+
+    //
+    $re_href = get_bloginfo('template_url') . '/assets/images/rounded_elements.svg';
+    $header_text = get_field('header_text');
+    //
+    $title_decoration_360_href = get_bloginfo('template_url') . '/assets/images/title_element_360.svg';
 ?>
 <section class="section <?= $classes["section"] ?>">
-    <div class="container <?= $classes["container"] ?>" id="eventsSliderContainer" data-ajax-url="<?= $ajax_url ?>">
-        
+    <div class="container <?= $classes["container"] ?>">
+        <div class="<?= $classes["title"] ?>">
+            <div class="<?= $classes["title-header"] ?>">
+                <img class="<?= $classes["title-header-image"]?>" src="<?= $re_href ?>">
+                <h2 class="<?= $classes["title-header-text"] ?>"><?= $header_text ?></h2>
+                <img class="<?= $classes["title-header-image"]?>" src="<?= $re_href ?>">
+            </div>
+            <img class="<?= $classes["title-decoration-image"] ?>" src="<?= $title_decoration_360_href ?>">
+        </div>
+
+        <?php get_template_part('inc/acf/blocks/events-slider/items');?>
     </div>
 </section>
--- inc/acf/blocks/events-slider/item.php
+++ inc/acf/blocks/events-slider/item.php
@@ -1,40 +1,77 @@
 <?php
+    // Helper class to work with images
+    require_once(get_template_directory() . '/inc/acf/blocks/events-slider/image-helper.php');
+
     // Loading classes
     $default_classes = [
-        'title' => 'title',
-        'title-header' => 'title-header',
-        'title-decoration-image' => 'title-decoration-image',
+        'item' => 'item',
+        'item-upper-part' => 'item-upper-part',
+        'item-upper-part-image' => 'item-upper-part-image',
     ];
 
     $modules_file = get_template_directory() . '/assets/blocks/styles/modules.json';
     $classes = $default_classes;
     if (file_exists($modules_file)) {
         $modules = json_decode(file_get_contents($modules_file), true);
-        $classes = array_merge($default_classes, $modules['events'] ?? []);
+        $classes = array_merge($default_classes, $modules['events-slider'] ?? []);
     }
 
-    //
-    $ajax_url = admin_url('admin-ajax.php');
+    // Retrieving the texts from the arguments
+    $price_label_text = $args['price_label_text'];
+    $duration_label_text = $args['duration_label_text'];
+    $registration_button_text = $args['registration_button_text'];
+    $show_more_text = $args['show_more_text'];
+    $show_less_text = $args['show_less_text'];
 
-    //
-    $re_href = get_bloginfo('template_url') . '/assets/images/rounded_elements.svg';
-    $header_text = get_field('header_text');
-    //
-    $title_decoration_360_href = get_bloginfo('template_url') . '/assets/images/title_element_360.svg';
+
+    // Preparing some values
+    $current_id = get_the_ID();
+    // The image
+    $image = get_field('image', $current_id);
+    $image_tag = ImageHelper::get_image_tag(
+        $image, 
+        $classes['item-upper-part-image'],
+        '(max-width: 767.9px) 100vw, (max-width: 1439.9px) 50vw, 33.33vw'
+    );
+
+    // The date
+    $date = get_field('date', $current_id);
+    // Putting all the dates into the array, sorting them, then converting them into the strings in format 'd.m.Y' and imploding them
+    // with ', ' separator
+    $dates = [\DateTime::createFromFormat('d.m.Y', $date)];
+    while (have_rows('additional_dates', $current_id)) {
+        the_row();
+
+        $next_date = get_sub_field('date');
+        $dates[] = \DateTime::createFromFormat('d.m.Y', $next_date);
+    }
+    sort($dates);
+    $formatted_dates = array_map(function ($date) {
+        return $date->format('d.m.Y');
+    }, $dates);
+    $dates_to_display = implode(', ', $formatted_dates);
+
+    // The time
+    $start_time = get_field('start_time', $current_id);
+    $end_time = get_field('end_time', $current_id);
+    $time = $start_time . ($end_time ? "-$end_time" : '');
+    // The register link
+    $register_link = get_field('register_link', $current_id);
+    $show_register_button = false;
+
+    if ($register_link) {
+        //
+        $start_datetime = \DateTime::createFromFormat('d.m.Y H.i', "$date $start_time");
+        //
+        $now_datetime = new DateTime();
+
+        $show_register_button = $start_datetime > $now_datetime;
+    }
+    // The main text
+    $main_text = acf_esc_html(get_field('main_text', $current_id));
 ?>
-    <div class="upper-part">
-        <!-- Title -->
-        <div class="<?= $classes["title"] ?>">
-            <div class="<?= $classes["title-header"] ?>">
-                <img src="<?= $re_href ?>">
-                <h2><?= $header_text ?></h2>
-                <img src="<?= $re_href ?>">
-            </div>
-            <img class="<?= $classes["title-decoration-image"] ?>" src="<?= $title_decoration_360_href ?>">
-        </div>
-        <!-- Image -->
-        <!-- Swiper controls -->
+<div class="<?= $classes["item"] ?>">
+    <div class="<?= $classes["item-upper-part"] ?>">
+        123
     </div>
-    <div class="text-part">
-
-    </div>
+</div>
