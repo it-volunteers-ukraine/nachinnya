9b97c197bb019034ad10ea20443da5da70aff30f
kadulin <kadulin@bk.ru>
2025-03-30 14:52:58 +0200

    header 1440

--- footer.php
+++ footer.php
@@ -36,68 +36,69 @@ $socials = get_field('socials', 'options');
     }
 </style>
 
-<footer>
-    <div class="ornament"></div>
-    <div class="footer">
-        <div class="footer-content">
-            <a href="/" class="logo-footer-link">
-                <svg class="logo-footer-img">
-                    <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-logo-white"></use>
-                </svg>
-            </a>
-            <?php
-            // if (has_nav_menu('footer-menu')) {
-            wp_nav_menu(array(
-                'theme_location' => 'footer',
-                'menu_class'     => 'footer-menu', // Класс для стилизации меню
-                'container'      => false, // Убираем контейнер (если не нужен)
-                "menu_id"        => "footer-menu", //
-                'items_wrap'      => '<ul id="%1$s" class="%2$s wrap-link-adv">%3$s</ul>',
-                // 'list_item_class'  => 'nav-item',
-                // 'link_class' => 'aaaaa',
-            ));
-            // }
-            ?>
-
-            <address class="address">
-                <div class="address-contact">
-                    <div class="address-wrap ">
-                        <a href="mailto:<?php echo $email; ?>" target="_blank" class="email link-adv"><?php echo $email; ?></a>
-                    </div>
-                    <div class="address-wrap ">
-                        <a href="tel:<?php echo $phone; ?>" class="phone link-adv"><?php echo $phone_number; ?></a>
-                    </div>
-                </div>
-                <div class="uzor">
-                    <svg class="wave-y hidden">
-                        <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-wave-m-y"></use>
+    <footer>
+        <div class="ornament"></div>
+        <div class="footer">
+            <div class="footer-content">
+                <a href="/" class="logo-footer-link">
+                    <svg class="logo-footer-img">
+                        <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-logo-white"></use>
                     </svg>
-                    <svg class="wave-y hidden">
-                        <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-wave-l-y"></use>
-                    </svg>
-                </div>
+                </a>
                 <?php
-                get_template_part('template-parts/socblock', null, ['is_main' => false, 'is_title' => true]);
+                // if (has_nav_menu('footer-menu')) {
+                wp_nav_menu(array(
+                    'theme_location' => 'footer',
+                    'menu_class'     => 'footer-menu', // Класс для стилизации меню
+                    'container'      => false, // Убираем контейнер (если не нужен)
+                    "menu_id"        => "footer-menu", //
+                    'items_wrap'      => '<ul id="%1$s" class="%2$s wrap-link-adv">%3$s</ul>',
+                    // 'list_item_class'  => 'nav-item',
+                    // 'link_class' => 'aaaaa',
+                ));
+                // }
                 ?>
 
-            </address>
+                <address class="address">
+                    <div class="address-contact">
+                        <div class="address-wrap ">
+                            <a href="mailto:<?php echo $email; ?>" target="_blank" class="email link-adv"><?php echo $email; ?></a>
+                        </div>
+                        <div class="address-wrap ">
+                            <a href="tel:<?php echo $phone; ?>" class="phone link-adv"><?php echo $phone_number; ?></a>
+                        </div>
+                    </div>
+                    <div class="uzor">
+                        <svg class="wave-y hidden">
+                            <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-wave-m-y"></use>
+                        </svg>
+                        <svg class="wave-y hidden">
+                            <use xlink:href="<?php bloginfo('template_url'); ?>/assets/images/symbol-defs.svg#icon-wave-l-y"></use>
+                        </svg>
+                    </div>
+                    <?php
+                    get_template_part('template-parts/socblock', null, ['is_main' => false, 'is_title' => true]);
+                    ?>
 
-        </div>
+                </address>
 
-        <div class="copyright-wrap">
-            <p>
-                <?php echo (get_field('copyright_one_part', 'option')); ?>
-                <a href="https://it-volunteers.com/" class="copyright link-adv" target="_blank">IT Volunteers</a>
-                &nbsp
-            </p>
-            <p>
-                &copy;<?php echo date('Y'); ?>
-                <?php echo (get_field('copyright_two_part', 'option')); ?>
-            </p>
+            </div>
+
+            <div class="copyright-wrap">
+                <p>
+                    <?php echo (get_field('copyright_one_part', 'option')); ?>
+                    <a href="https://it-volunteers.com/" class="copyright link-adv" target="_blank">IT Volunteers</a>
+                    &nbsp
+                </p>
+                <p>
+                    &copy;<?php echo date('Y'); ?>
+                    <?php echo (get_field('copyright_two_part', 'option')); ?>
+                </p>
+            </div>
         </div>
-    </div>
-</footer>
-<?php wp_footer(); ?>
+    </footer>
+    <?php wp_footer(); ?>
+</div>
 </body>
 
 </html>
